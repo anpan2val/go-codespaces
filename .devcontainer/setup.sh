076fcf2c892bad3b5a6b16803919b3111e8d1883
anpan2val <121332155+anpan2val@users.noreply.github.com>
2023-10-29 11:32:44 +0000

    change go install

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,16 +12,16 @@ ARCH='amd64'
 
 curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
 tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
+# rm go$VERSION.$OS-$ARCH.tar.gz
 
 INSTALLED_GO_VERSION=$(go version)
 echo "Go version ${INSTALLED_GO_VERSION} is installed"
 
-# install gopls, dlv, hey
-echo "Getting development tools"
-go get -u golang.org/x/tools/gopls
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/rakyll/hey
+# # install gopls, dlv, hey
+# echo "Getting development tools"
+# go get -u golang.org/x/tools/gopls
+# go get -u github.com/go-delve/delve/cmd/dlv
+# go get -u github.com/rakyll/hey
 
 # # vscode-go dependencies 
 # echo "Getting dependencies for the vscode-go plugin "
